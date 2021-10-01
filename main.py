be7e9f2d7f970a6d1c88dfbb77e197ee452c8289
Akshaya H <81792372+my-my414@users.noreply.github.com>
2021-10-01 09:24:39 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+//reverse a number
+n=int(input("please give a number:")
+reverse=0
+while n!=0:
+         reverse=reverse*	10+n%10
+        n=(n//	10)
+print("after reverse:")
+//reverse a string
+txt="akshaya"[::-1]
+print(txt)
